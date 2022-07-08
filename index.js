624773e16013d3cdfa810c2122930a85154d5077
Akp-108 <akp1081@gmail.com>
2022-07-08 20:12:53 +0530

    test

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const cors = require("cors")
 const app = express()
 
 app.use(cors())
-app.use('/',function(req,res){
-    res.send('function is running')
-})
+// app.use('/',function(req,res){
+//      res.send('function is running')
+//  })
 app.use("/api",detalisRouter1)
 app.use("/api",detalisRouter2)
 
-app.listen(3001,()=>{
+app.listen(process.env.PORT||8000,()=>{
     console.log("app is running");
 })
